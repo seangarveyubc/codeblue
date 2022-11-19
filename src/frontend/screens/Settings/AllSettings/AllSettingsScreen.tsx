1d7b077fca52c0aadfe3aec3c1ca523d9a1b6c30
stellaw1 <stella01wang@gmail.com>
2022-11-19 03:14:54 -0500

    finish touching up settings option

--- src/frontend/screens/Settings/AllSettings/AllSettingsScreen.tsx
+++ src/frontend/screens/Settings/AllSettings/AllSettingsScreen.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { Button, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import Colours from '../../../../utilities/Colours';
 import { SettingsOption } from './SettingsOption';
+
 
 interface Props {
     navigation: any,
@@ -11,7 +15,8 @@ export const AllSettingsScreen = ({ navigation }: Props) => {
         <View>
             <Text>All settings screen</Text>
             <SettingsOption navigation={navigation} title={"Account Information"} url={'AccountInfo'} />
-            <Button title='Medical info' onPress={() => { navigation.navigate('MedicalInfo')}} />
+            <SettingsOption navigation={navigation} title={"Medical Information"} url={'MedicalInfo'} />
+            <Icon name="briefcase-medical" size={30} color={Colours.DARKBLUE} />
         </View>
     );
 };
