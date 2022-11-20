ac11588f22950aedb9148c57e84862bb36857336
Emily Lukas <41706038+emilylukas@users.noreply.github.com>
2022-11-20 15:59:58 -0800

    Checkbox component (#7)
    * Add checkbox component
    
    * Use colour constants
    
    * Cleanup and prettier

--- src/frontend/screens/Onboarding/OptionalInfo/OptionalInfoScreen.tsx
+++ src/frontend/screens/Onboarding/OptionalInfo/OptionalInfoScreen.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
-import { Button, Text } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 import { CentredContent } from '../../../components/CentredContent';
+import { CheckBox } from '../../../components/CheckBox';
 
 interface Props {
     navigation: any;
 }
 
 export const OptionalInfoScreen = ({ navigation }: Props) => {
+    const [isSelected, setIsSelected] = useState(false);
+
     return (
         <CentredContent>
             <Text>Onboarding optional info screen</Text>
             <Button
-                title="next"
+                title='next'
                 onPress={() => {
                     navigation.navigate('RequiredInfo');
                 }}
