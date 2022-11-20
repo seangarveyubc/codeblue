b7bb073f5418cefc825b225eaa97b414775dde84
Akash Randhawa <akashrandhawa@hotmail.com>
2022-11-19 17:52:43 -0800

    Formatted with prettier

--- src/frontend/components/TextInputField.tsx
+++ src/frontend/components/TextInputField.tsx
@@ -3,50 +3,50 @@ import { Image, StyleSheet, TextInput, View } from "react-native";
 import Colours from "../../utilities/Colours";
 
 interface Props {
-  text:any,
-  onChangeText:any
+  text: any;
+  onChangeText: any;
 }
 
 const IconTextInput = ({ text, onChangeText }: Props) => {
-
   return (
     <View style={styles.inputfield}>
-        <Image
-        source={require('../assets/images/connected.png')} //Change your icon image here
+      <Image
+        source={require("../assets/images/connected.png")} //Change your icon image here
         style={styles.ImageStyle}
-        />
-        <TextInput
+      />
+      <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
-        placeholder="place holder"
+        placeholder="placeholder"
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  inputfield:{
-    flexDirection: 'row',
-    flexWrap:'wrap',
-    justifyContent: 'flex-start',
-    alignContent: 'center',
-    alignItems: 'center',
-    margin:10,
-    height:60,
+  inputfield: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignContent: "center",
+    alignItems: "center",
+    margin: 10,
+    height: 60,
     borderWidth: 2,
     borderRadius: 10,
     borderColor: Colours.BLUE,
   },
   input: {
-    alignContent: 'center'
+    alignContent: "center",
+    fontFamily: "DM SANS"
   },
   ImageStyle: {
-    height:20,
-    width:20,
-    margin:8,
-    resizeMode:'contain'
-  }
+    height: 20,
+    width: 20,
+    margin: 8,
+    resizeMode: "contain",
+  },
 });
 
-export default IconTextInput; 
+export default IconTextInput;
