f6d9975bc230cf574cb69ae45ab90c5db2e357e6
Emily Lukas <41706038+emilylukas@users.noreply.github.com>
2022-11-20 22:39:15 -0800

    Bottom nav bar (#16)
    * Bottom nav bar
    
    * Cleanup

--- src/frontend/navigation/MainNavigator.tsx
+++ src/frontend/navigation/MainNavigator.tsx
@@ -3,6 +3,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from '../screens/Home/HomeScreen';
 import { AddDeviceScreen } from '../screens/AddDevice/AddDeviceScreen';
 import { SettingsStack } from './SettingsStack';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Colours from '../../utilities/Colours';
 
 const NavBar = createBottomTabNavigator();
 
@@ -10,11 +14,62 @@ export const MainNavigator = () => {
     return (
         <NavBar.Navigator
             initialRouteName="Home"
-            screenOptions={{ headerShown: false }}
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: Colours.BLUE,
+                tabBarInactiveTintColor: Colours.GREY,
+                tabBarStyle: {
+                    backgroundColor: Colours.WHITE,
+                    height: 65,
+                    justifyContent: 'center'
+                },
+                tabBarIconStyle: {
+                    marginTop: 10
+                },
+                tabBarLabelStyle: {
+                    marginBottom: 10
+                },
+                tabBarLabelPosition: 'below-icon'
+            }}
         >
-            <NavBar.Screen name="Home" component={HomeScreen} />
-            <NavBar.Screen name="AddDevice" component={AddDeviceScreen} />
-            <NavBar.Screen name="Settings" component={SettingsStack} />
+            <NavBar.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcon
+                            name="home"
+                            color={color}
+                            size={28}
+                        />
+                    )
+                }}
+            />
+            <NavBar.Screen
+                name="Add Device"
+                component={AddDeviceScreen}
+                options={{
+                    tabBarIcon: () => (
+                        <AntDesign
+                            name="pluscircle"
+                            color={Colours.DARKBLUE}
+                            size={65}
+                        />
+                    ),
+                    tabBarIconStyle: {
+                        marginTop: -33
+                    }
+                }}
+            />
+            <NavBar.Screen
+                name="Settings"
+                component={SettingsStack}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcon name="settings" color={color} size={28} />
+                    )
+                }}
+            />
         </NavBar.Navigator>
     );
 };
