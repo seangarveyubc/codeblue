7f6102f06fdab667eab7ce3a3d3875078f9a69e2
stellaw1 <stella01wang@gmail.com>
2022-11-20 22:51:09 -0500

    merges main and updates font

--- src/frontend/components/UnderLineTextInput.tsx
+++ src/frontend/components/UnderLineTextInput.tsx
@@ -1,53 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import Colours from "../../utilities/Colours";
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Colours from '../../utilities/Colours';
 
 interface Props {
-  text: string;
-  onChangeText: any;
-  title: string;
-  placeholder: string;
+    text: string;
+    onChangeText: any;
+    title: string;
+    placeholder: string;
 }
 
 const UnderlineTextInput = ({
-  text,
-  onChangeText,
-  title,
-  placeholder,
+    text,
+    onChangeText,
+    title,
+    placeholder
 }: Props) => {
-  return (
-    <View style={styles.inputfield}>
-      <Text style={styles.title}>{title}</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder={placeholder}
-        placeholderTextColor={Colours.GREY}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.inputfield}>
+            <Text style={styles.title}>{title}</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder={placeholder}
+                placeholderTextColor={Colours.GREY}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  inputfield: {
-    flexDirection: "column",
-    justifyContent: "space-evenly",
-    margin: 10,
-    height: 50,
-    borderBottomWidth: 1,
-    borderBottomColor: Colours.GREY,
-  },
-  input: {
-    fontFamily: "DM SANS",
-    fontSize: 18,
-  },
-  title: {
-    fontFamily: "DM SANS",
-    paddingLeft:4,
-    color: Colours.BLUE,
-    fontSize: 15,
-  },
+    inputfield: {
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        margin: 10,
+        height: 50,
+        borderBottomWidth: 1,
+        borderBottomColor: Colours.GREY
+    },
+    input: {
+        fontFamily: 'DMSans-Regular',
+        fontSize: 18
+    },
+    title: {
+        fontFamily: 'DMSans-Regular',
+        paddingLeft: 4,
+        color: Colours.BLUE,
+        fontSize: 15
+    }
 });
 
 export default UnderlineTextInput;
