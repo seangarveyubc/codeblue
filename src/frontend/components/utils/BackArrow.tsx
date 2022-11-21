4f1f0f5034c0b9c8af2ea781121b1d774b7b0ee6
gurmanstoor <52118935+gurmanstoor@users.noreply.github.com>
2022-11-20 16:49:26 -0800

    Radio buttons (#4)

--- src/frontend/components/utils/BackArrow.tsx
+++ src/frontend/components/utils/BackArrow.tsx
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 18,
-        fontFamily: 'DM Sans',
+        fontFamily: 'DMSans-Regular', 
         fontWeight: '700',
         color: Colours.BLUE,
         alignItems: 'center'
