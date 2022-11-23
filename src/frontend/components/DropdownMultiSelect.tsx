ee193872e561d57f7ea7a1ecf94465b71974b68d
Sean Garvey <sgarveybc@gmail.com>
2022-11-23 15:55:20 -0800

    Updated formatting

--- src/frontend/components/DropdownMultiSelect.tsx
+++ src/frontend/components/DropdownMultiSelect.tsx
@@ -1,88 +1,87 @@
 import React, { useState } from 'react';
-  import { StyleSheet, Text, View } from 'react-native';
-  import { MultiSelect } from 'react-native-element-dropdown';
-  import AntDesign from 'react-native-vector-icons/AntDesign';
+import { StyleSheet, Text, View } from 'react-native';
+import { MultiSelect } from 'react-native-element-dropdown';
 import Colours from "../../utilities/Colours";
 
 
-  interface Props {
+interface Props {
     placeholder: string
     width: number
     data: any
-  };
+};
 
 const DropdownMultiSelect = ({ placeholder, width , data}) => {
     const [selected, setSelected] = useState([]);
 
     return (
-      <View style={[styles.container, {width: width}]}>
-        <MultiSelect
-          style={styles.dropdown}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          search
-          data={data}
-          labelField="label"
-          valueField="value"
-          placeholder={placeholder}
-          searchPlaceholder="Search..."
-          value={selected}
-          onChange={item => {
-            setSelected(item);
-          }}
-          selectedStyle={styles.selectedStyle}
-        />
-      </View>
+        <View style={[styles.container, {width: width}]}>
+            <MultiSelect
+                style={styles.dropdown}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                search
+                data={data}
+                labelField="label"
+                valueField="value"
+                placeholder={placeholder}
+                searchPlaceholder="Search..."
+                value={selected}
+                onChange={item => {
+                    setSelected(item);
+                }}
+                selectedStyle={styles.selectedStyle}
+            />
+        </View>
     );
-  };
+};
 
-  export default DropdownMultiSelect;
+export default DropdownMultiSelect;
 
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
-      padding: 16,
+        padding: 16,
     },
     dropdown: {
-      height: 56,
-      borderColor: Colours.BLUE,
-      borderWidth: 1,
-      borderRadius: 8,
-      paddingHorizontal: 8,
+          height: 56,
+          borderColor: Colours.BLUE,
+          borderWidth: 1,
+          borderRadius: 8,
+          paddingHorizontal: 8,
     },
     icon: {
-      marginRight: 5,
-      color: Colours.BLUE
+          marginRight: 5,
+          color: Colours.BLUE
     },
     label: {
-      position: 'absolute',
-      color: Colours.BLUE,
-      left: 22,
-      top: 8,
-      zIndex: 999,
-      paddingHorizontal: 8,
-      fontSize: 14,
+          position: 'absolute',
+          color: Colours.BLUE,
+          left: 22,
+          top: 8,
+          zIndex: 999,
+          paddingHorizontal: 8,
+          fontSize: 14,
     },
     placeholderStyle: {
           fontSize: 16,
           color: Colours.BLUE,
           fontFamily: 'DMSans-Regular'
-        },
-        selectedTextStyle: {
+    },
+    selectedTextStyle: {
           fontSize: 16,
           fontFamily: 'DMSans-Regular',
           color: Colours.BLUE,
-        },
-        selectedStyle: {
+    },
+    selectedStyle: {
           borderRadius: 8,
           backgroundColor: Colours.WHITE,
           borderColor: Colours.DARKGREY
-        },
-    inputSearchStyle: {
-      height: 40,
-      fontSize: 16,
-      color: Colours.BLUE,
-      fontFamily: 'DMSans-Regular'
     },
-  });
+    inputSearchStyle: {
+          height: 40,
+          fontSize: 16,
+          color: Colours.BLUE,
+          fontFamily: 'DMSans-Regular'
+    },
+});
--- src/frontend/components/DropdownSingleSelect.tsx
+++ src/frontend/components/DropdownSingleSelect.tsx
@@ -1,67 +1,65 @@
-  import React, { useState } from 'react';
-  import { StyleSheet, View } from 'react-native';
-  import { Dropdown } from 'react-native-element-dropdown';
-  import AntDesign from 'react-native-vector-icons/AntDesign';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
 
-  import Colours from  "../../utilities/Colours";
+import Colours from  "../../utilities/Colours";
 
+interface Props {
+    data: any
+    placeholder: string
+    width: number
+};
 
-    interface Props {
-        data: any
-        placeholder: string
-        width: number
-    };
-
-  const DropdownSingleSelect = ({ width, placeholder, data}: Props) => {
+const DropdownSingleSelect = ({ width, placeholder, data}: Props) => {
     const [selected, setSelected] = useState([]);
 
     return (
-      <View style={[styles.container,  {width: width}]}>
-        <Dropdown
-          style={styles.dropdown}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={data}
-          labelField="label"
-          valueField="value"
-          placeholder={placeholder}
-          value={selected}
-          onChange={item => {
-            setSelected(item);
-          }}
-          selectedStyle={styles.selectedStyle}
-        />
-      </View>
+        <View style={[styles.container,  {width: width}]}>
+            <Dropdown
+                style={styles.dropdown}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={data}
+                labelField="label"
+                valueField="value"
+                placeholder={placeholder}
+                value={selected}
+                onChange={item => {
+                    setSelected(item);
+                }}
+                selectedStyle={styles.selectedStyle}
+            />
+        </View>
     );
-  };
+};
 
-  export default DropdownSingleSelect;
+export default DropdownSingleSelect;
 
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         padding: 16
     },
     dropdown: {
-      height: 56,
-      backgroundColor: Colours.LIGHTGREY,
-      borderColor: Colours.BLUE,
-      borderWidth: 1,
-      borderRadius: 8,
-      padding: 10,
+        height: 56,
+        backgroundColor: Colours.LIGHTGREY,
+        borderColor: Colours.BLUE,
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 10,
     },
     placeholderStyle: {
-      fontSize: 16,
-      color: Colours.BLUE,
-      fontFamily: 'DMSans-Regular'
+        fontSize: 16,
+        color: Colours.BLUE,
+        fontFamily: 'DMSans-Regular'
     },
     selectedTextStyle: {
-      fontSize: 16,
-      fontFamily: 'DMSans-Regular',
-      color: Colours.BLUE,
+        fontSize: 16,
+        fontFamily: 'DMSans-Regular',
+        color: Colours.BLUE,
     },
     selectedStyle: {
-      borderRadius: 8,
-    },
-  });
+        borderRadius: 8,
+    }
+});
