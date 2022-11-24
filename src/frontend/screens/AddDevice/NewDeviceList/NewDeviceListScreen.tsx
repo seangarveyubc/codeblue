d205d577927737c5697b15382010e5e2a7627ac2
Gurman Toor <gurman.toor99@gmail.com>
2022-11-23 22:38:31 -0800

    Added new device widget component and new device listing and adding screen

--- src/frontend/screens/AddDevice/NewDeviceList/NewDeviceListScreen.tsx
+++ src/frontend/screens/AddDevice/NewDeviceList/NewDeviceListScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import Colours from '../../../../utilities/Colours'
+import Colours from '../../../../utilities/Colours';
+import {AddDeviceWidget} from '../../../components/AddDeviceWidget'
 
 interface Props {
     navigation: any;
@@ -10,6 +11,11 @@ export const NewDeviceListScreen = ({ navigation }: Props) => {
     return (
     <View style={styles.page}>
         <Text style={styles.title}>Add New Devices</Text>
+        <View style={styles.devicelist}>
+            <AddDeviceWidget name="default 1"></AddDeviceWidget>
+            <AddDeviceWidget name="default 2"></AddDeviceWidget>
+            <AddDeviceWidget name="default 3"></AddDeviceWidget>
+        </View>    
     </View>
 );
 };
@@ -20,10 +26,14 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         backgroundColor: Colours.WHITE
     },
+    devicelist: {
+        alignItems: 'center',
+    },
     title: {
         fontFamily: 'DMSans-Bold',
         marginLeft: 20,
         marginTop: 30,
+        marginBottom: 30,
         color: Colours.BLACK,
         fontSize: 24
     },
