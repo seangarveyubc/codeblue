da8cee9cbdf3307a37fab7dd68f869a6bfc26147
gurmanstoor <52118935+gurmanstoor@users.noreply.github.com>
2022-11-24 15:11:11 -0800

    Added Legal Screen and updated navigation stack to go to legal screen (#26)

--- src/frontend/navigation/SettingsStack.tsx
+++ src/frontend/navigation/SettingsStack.tsx
@@ -4,6 +4,7 @@ import { AllSettingsScreen } from '../screens/Settings/AllSettings/AllSettingsSc
 import { AccountInfoScreen } from '../screens/Settings/AccountInfo/AccountInfoScreen';
 import { MedicalInfoScreen } from '../screens/Settings/MedicalInfo/MedicalInfoScreen';
 import { TutorialScreen } from '../screens/Onboarding/Tutorial/TutorialScreen';
+import {LegalScreen} from '../screens/Settings/Legal/LegalScreen'
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ export const SettingsStack = () => {
             <Stack.Screen name="AllSettings" component={AllSettingsScreen} />
             <Stack.Screen name="AccountInfo" component={AccountInfoScreen} />
             <Stack.Screen name="MedicalInfo" component={MedicalInfoScreen} />
+            <Stack.Screen name="Legal" component={LegalScreen} />
             <Stack.Screen name="Tutorial" component={TutorialScreen} />
         </Stack.Navigator>
     );
