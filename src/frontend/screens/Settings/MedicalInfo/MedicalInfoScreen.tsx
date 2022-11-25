273e90a4ee550c60d98686d26fb42006d953a36c
Akash Randhawa <akashrandhawa@hotmail.com>
2022-11-24 18:55:41 -0800

    Created the account information and medical infromation screens

--- src/frontend/screens/Settings/MedicalInfo/MedicalInfoScreen.tsx
+++ src/frontend/screens/Settings/MedicalInfo/MedicalInfoScreen.tsx
@@ -1,14 +1,68 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Colours from '../../../../utilities/Colours';
+import { SettingsScreenHeader } from '../../../components/SettingsScreenHeader';
+import { UserMedicalInfo } from './UserMedicalInfo';
 
 interface Props {
     navigation: any;
 }
 
 export const MedicalInfoScreen = ({ navigation }: Props) => {
+    const [edit, setEdit] = React.useState(false);
+    const [personalHistory, setPersonalHistory] = React.useState('');
+    const [familyHistory, setFamilyHistory] = React.useState('');
+    const onPress = () => {
+        setEdit(!edit);
+    };
     return (
-        <View>
-            <Text>Medical info screen</Text>
+        <View style={styles.container}>
+            <SettingsScreenHeader title="Medical Information" />
+            <View style={styles.subHeading}>
+                <Text style={styles.subHeadingText}>{'Medical History'}</Text>
+                <Text style={styles.edit} onPress={onPress}>
+                    {edit ? 'Save' : 'Edit'}
+                </Text>
+            </View>
+
+            <UserMedicalInfo
+                edit={edit}
+                personalHistory={personalHistory}
+                familyHistory={familyHistory}
+                setPersonalHistory={setPersonalHistory}
+                setFamilyHistory={setFamilyHistory}
+            ></UserMedicalInfo>
+
+            <View style={styles.subHeading}>
+                <Text style={styles.subHeadingText}>{'Medication'}</Text>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: { height: '100%', backgroundColor: Colours.WHITE },
+    title: {
+        fontSize: 28,
+        fontFamily: 'DMSans-Bold',
+        alignSelf: 'center',
+        color: Colours.WHITE
+    },
+    subHeading: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: '5%',
+        marginHorizontal: '3%'
+    },
+    subHeadingText: {
+        fontSize: 20,
+        fontFamily: 'DMSans-Bold',
+        color: Colours.BLACK
+    },
+    edit: {
+        fontSize: 20,
+        fontFamily: 'DMSans-Bold',
+        color: Colours.BLUE
+    }
+});
