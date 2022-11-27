40279620c10d89ed9ef891be407dc04fb5c47586
seangarveyubc <seangarveyubc@users.noreply.github.com>
2022-11-27 01:09:00 +0000

    Prettified Code!

--- src/frontend/screens/Home/HomeScreen.tsx
+++ src/frontend/screens/Home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 import Colours from '../../../utilities/Colours';
 import { Logo } from '../../components/utils/Logo';
@@ -14,59 +14,80 @@ interface Props {
     navigation: any;
 }
 
-
 export const HomeScreen = ({ navigation }: Props) => {
     const [deviceListState, setDeviceListState] = useState(true);
     const [bluetoothState, setBluetoothState] = useState(false);
-    const[deviceName1, changeDeviceName1] = useState("PPG1");
-    const[deviceName2, changeDeviceName2] = useState("EKG1");
-    const toggleChecked = () => setDeviceListState(value => !value);
+    const [deviceName1, changeDeviceName1] = useState('PPG1');
+    const [deviceName2, changeDeviceName2] = useState('EKG1');
+    const toggleChecked = () => setDeviceListState((value) => !value);
 
     return (
         <View style={styles.container}>
             <View style={styles.swirl}>
-                <Swirl rotation={180}/>
+                <Swirl rotation={180} />
             </View>
             <View style={styles.container}>
                 <View style={styles.title}>
                     <View style={styles.logo}>
-                        <Logo
-                            width={35}
-                            height={35}/>
+                        <Logo width={35} height={35} />
                     </View>
                     <Text style={styles.codeblue}>CodeBlue</Text>
                 </View>
                 <Text style={styles.name}>Test Name</Text>
                 <View style={styles.heartContainer}>
-                    <HeartRateWidget heartRate={56}/>
+                    <HeartRateWidget heartRate={56} />
                 </View>
             </View>
             <View style={styles.devicesContainer}>
                 <View style={styles.deviceHeader}>
                     <Text style={styles.yourDevices}>Your Devices</Text>
-                    <Text style={styles.edit} onPress={toggleChecked}>{deviceListState ? 'Edit' : 'Save'}</Text>
+                    <Text style={styles.edit} onPress={toggleChecked}>
+                        {deviceListState ? 'Edit' : 'Save'}
+                    </Text>
                 </View>
                 {!bluetoothState ? (
                     <View style={styles.bluetoothPrompt}>
                         <CentredContent>
-                            <Text>CodeBlue requires Bluetooth to monitor heart rate.
-                                <Text style={{color:Colours.BLUE}}>Turn on Bluetooth.</Text>
+                            <Text>
+                                CodeBlue requires Bluetooth to monitor heart
+                                rate.
+                                <Text style={{ color: Colours.BLUE }}>
+                                    Turn on Bluetooth.
+                                </Text>
                             </Text>
                         </CentredContent>
                     </View>
                 ) : null}
                 {deviceListState ? (
-                    <View style={{flex:bluetoothState ? 7:6, marginTop: 10}}>
+                    <View
+                        style={{ flex: bluetoothState ? 7 : 6, marginTop: 10 }}
+                    >
                         <CentredContent>
-                            <DeviceWidget name={deviceName1} isConnected={true}/>
-                            <DeviceWidget name={deviceName2} isConnected={false}/>
+                            <DeviceWidget
+                                name={deviceName1}
+                                isConnected={true}
+                            />
+                            <DeviceWidget
+                                name={deviceName2}
+                                isConnected={false}
+                            />
                         </CentredContent>
                     </View>
                 ) : (
-                    <View style={{flex:bluetoothState ? 7:6, marginTop: 10}}>
+                    <View
+                        style={{ flex: bluetoothState ? 7 : 6, marginTop: 10 }}
+                    >
                         <CentredContent>
-                            <IconTextInput text={deviceName1} isConnected={true} onChangeText={text => changeDeviceName1(text)}/>
-                            <IconTextInput text={deviceName2} isConnected={false} onChangeText={text => changeDeviceName2(text)}/>
+                            <IconTextInput
+                                text={deviceName1}
+                                isConnected={true}
+                                onChangeText={(text) => changeDeviceName1(text)}
+                            />
+                            <IconTextInput
+                                text={deviceName2}
+                                isConnected={false}
+                                onChangeText={(text) => changeDeviceName2(text)}
+                            />
                         </CentredContent>
                     </View>
                 )}
@@ -87,7 +108,7 @@ const styles = StyleSheet.create({
         bottom: 0,
         left: 0,
         right: 0,
-        transform: [{scaleX: -1}]
+        transform: [{ scaleX: -1 }]
     },
     headerContainer: {
         flex: 1,
@@ -95,14 +116,14 @@ const styles = StyleSheet.create({
         backgroundColor: Colours.BLACK
     },
     heartContainer: {
-        flex: 3,
+        flex: 3
     },
     devicesContainer: {
         flex: 1
     },
     title: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'row'
     },
     name: {
         flex: 1,
@@ -112,20 +133,20 @@ const styles = StyleSheet.create({
         paddingTop: 5
     },
     logo: {
-        position:'absolute',
-        bottom:0
+        position: 'absolute',
+        bottom: 0
     },
     codeblue: {
         flex: 1,
         fontFamily: 'DMSans-Bold',
         fontSize: 28,
         color: Colours.BLUE,
-        position:'absolute',
-        bottom:0,
+        position: 'absolute',
+        bottom: 0,
         left: 50
     },
     yourDevices: {
-        flex:3,
+        flex: 3,
         fontFamily: 'DMSans-Bold',
         fontSize: 24,
         left: 20,
@@ -134,7 +155,7 @@ const styles = StyleSheet.create({
         bottom: 0
     },
     edit: {
-        flex:1,
+        flex: 1,
         fontFamily: 'DMSans-Bold',
         fontSize: 18,
         color: Colours.BLUE,
@@ -144,11 +165,11 @@ const styles = StyleSheet.create({
     },
     deviceHeader: {
         flex: 1,
-        flexDirection:'row',
+        flexDirection: 'row',
         paddingTop: 20
     },
     bluetoothPrompt: {
-        flex:1,
+        flex: 1,
         margin: 10
-    },
+    }
 });
