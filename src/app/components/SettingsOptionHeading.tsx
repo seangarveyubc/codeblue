6acfa7d5c5abc3a44caa429d468abb74338e745c
Stella Wang <46392676+stellaw1@users.noreply.github.com>
2022-11-27 15:46:27 -0800

    Refactor repo structure (#34)

---
+++ src/app/components/SettingsOptionHeading.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { TouchableRipple } from 'react-native-paper';
+import Colours from '../assets/constants/Colours';
+
+interface Props {
+    title: string;
+}
+
+export const SettingsOptionHeading = ({ title }: Props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.row}>
+                <Text style={styles.text}>{title}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        borderBottomWidth: 0.5,
+        borderColor: Colours.LIGHTGREY
+    },
+    row: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        height: 40,
+        marginHorizontal: 20
+    },
+    text: {
+        fontSize: 13,
+        color: Colours.BLACK,
+        alignItems: 'center',
+        fontFamily: 'DMSans-Bold'
+    }
+});
---
+++ src/app/components/SettingsScreenHeader.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+import Colours from '../assets/constants/Colours';
+import { BackArrow } from './utils/BackArrow';
+
+interface Props {
+    title: string;
+    navigation: any;
+}
+
+export const SettingsScreenHeader = ({ navigation, title }: Props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.semicircle}>
+                <View style={styles.header}>
+                    <BackArrow
+                        onPress={() => navigation.goBack()}
+                        label={'Back'}
+                        colour={Colours.WHITE}
+                    />
+                    <Text style={styles.text}>{title}</Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+const window = Dimensions.get('window');
+const styles = StyleSheet.create({
+    container: {
+        width: window.width,
+        overflow: 'hidden',
+        height: window.width / 3
+    },
+    semicircle: {
+        borderRadius: window.width,
+        width: window.width * 2.2,
+        height: window.width * 2,
+        marginLeft: -(window.width / 1.66),
+        position: 'absolute',
+        bottom: 0,
+        backgroundColor: Colours.DARKBLUE
+    },
+    header: {
+        alignSelf: 'center',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        height: window.width / 3,
+        width: window.width / 1.1,
+        position: 'absolute',
+        bottom: 0,
+        paddingTop: '5%',
+        paddingBottom: '10%',
+        flexDirection: 'column'
+    },
+    text: {
+        fontSize: 28,
+        fontFamily: 'DMSans-Bold',
+        alignSelf: 'center',
+        color: Colours.WHITE
+    }
+});
