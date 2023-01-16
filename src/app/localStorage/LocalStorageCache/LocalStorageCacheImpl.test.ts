9f446e7cc0dd14ec5e0c75c63096af6e6e07a935
emilylukas <emily.lukas@gmail.com>
2023-01-16 00:25:18 -0800

    Finish tests

--- src/app/localStorage/LocalStorageCache/LocalStorageCacheImpl.test.ts
+++ src/app/localStorage/LocalStorageCache/LocalStorageCacheImpl.test.ts
@@ -64,27 +64,48 @@ describe('LocalStorageCacheImpl', () => {
     });
 
     describe('refresh', () => {
-        it('does not delete any data less than 24 hours old', () => {
-            mmkv.getAllKeys.mockReturnValue(['key1 expires', 'key2 expires']);
+        it('does not delete any data if all entries are less than 24 hours old', () => {
+            mmkv.getAllKeys.mockReturnValue([
+                'key1-valid 1800000000000',
+                'key2-valid 1700000000000'
+            ]);
+            mmkv.contains.mockReturnValue(true);
             Date.now = jest.fn().mockReturnValue(1500000000000);
+
             cache.refresh();
+            expect(mmkv.delete).not.toHaveBeenCalled();
+        });
+
+        it('deletes data that is over 24 hours old', () => {
+            mmkv.getAllKeys.mockReturnValue([
+                'key1-valid 1600000000000',
+                'key2-expires 1400000000000'
+            ]);
+            mmkv.contains.mockReturnValue(true);
+            Date.now = jest.fn().mockReturnValue(1500000000000);
+
+            cache.refresh();
+            expect(mmkv.delete).toHaveBeenCalledTimes(1);
             expect(mmkv.delete).toHaveBeenCalledWith(
-                'key1 expires 1500000000000'
-            );
-            expect(mmkv.delete).toHaveBeenCalledWith(
-                'key2 expires 1500000000000'
+                'key2-expires 1400000000000'
             );
         });
 
-        it('deletes expired entries', () => {
-            mmkv.getAllKeys.mockReturnValue(['key1 expires', 'key2 expires']);
+        it('deletes all data if all entries are expired', () => {
+            mmkv.getAllKeys.mockReturnValue([
+                'key1-expires 1500000000000',
+                'key2-expires 1400000000000'
+            ]);
+            mmkv.contains.mockReturnValue(true);
             Date.now = jest.fn().mockReturnValue(1500000000000);
+
             cache.refresh();
+            expect(mmkv.delete).toHaveBeenCalledTimes(2);
             expect(mmkv.delete).toHaveBeenCalledWith(
-                'key1 expires 1500000000000'
+                'key1-expires 1500000000000'
             );
             expect(mmkv.delete).toHaveBeenCalledWith(
-                'key2 expires 1500000000000'
+                'key2-expires 1400000000000'
             );
         });
     });
