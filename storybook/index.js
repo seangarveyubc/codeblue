88196937aad239791257163a44293c5975beef72
stellaw1 <stella01wang@gmail.com>
2023-01-17 22:21:22 -0800

    minor undo edit

--- storybook/index.js
+++ storybook/index.js
@@ -22,10 +22,7 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({
-    host: '192.168.0.42', // replace this with your local ip address
-    port: '7007'
-});
+const StorybookUIRoot = getStorybookUI({});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
