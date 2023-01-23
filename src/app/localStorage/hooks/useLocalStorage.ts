5e0eb22a3029a10cec9f24faa96cbe56633d4086
emilylukas <emily.lukas@gmail.com>
2023-01-23 13:49:37 -0800

    Update storage names

--- src/app/localStorage/hooks/useLocalStorage.ts
+++ src/app/localStorage/hooks/useLocalStorage.ts
@@ -1,8 +1,8 @@
 import { LocalStorage } from '../LocalStorage/LocalStorage';
 import { LocalStorageCache } from '../LocalStorageCache/LocalStorageCache';
 
-export const APP_DATA_STORAGE = 'test1';
-export const CARDIAC_STORAGE = 'test2';
+export const APP_DATA_STORAGE = 'APP_DATA_STORAGE';
+export const CARDIAC_STORAGE = 'CARDIAC_STORAGE';
 
 const appDataStorage = new LocalStorage(APP_DATA_STORAGE);
 const cardiacStorage = new LocalStorageCache(CARDIAC_STORAGE);
