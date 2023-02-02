7afc382dad042877b88ccc367152799c2c452a92
Stella Wang <46392676+stellaw1@users.noreply.github.com>
2023-02-02 12:32:57 -0800

    Integrates FCM notifications (#43)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppNavigator } from './src/app/navigation/AppNavigator';
+import messaging from '@react-native-firebase/messaging';
 import StorybookUI from './storybook';
+import { Alert } from 'react-native';
 
 const App = () => {
+    useEffect(() => {
+        messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
+            console.log(remoteMessage);
+
+            let message_body = remoteMessage.notification.body;
+            let message_title = remoteMessage.notification.title;
+            let avatar = remoteMessage.notification.android.imageUrl;
+
+            Alert.alert(message_title, message_body);
+        });
+    }, []);
+
+    useEffect(() => {
+        const subscribe = messaging().onMessage(async (remoteMessage: any) => {
+            console.log(remoteMessage);
+
+            let message_body = remoteMessage.notification.body;
+            let message_title = remoteMessage.notification.title;
+            let avatar = remoteMessage.notification.android.imageUrl;
+
+            Alert.alert(message_title, message_body);
+        });
+
+        return subscribe;
+    }, []);
+
     return <AppNavigator />;
 };
 
