9a3fcc6eb653f0ae2f1b0104b5f9f5183e676d89
emilylukas <emily.lukas@gmail.com>
2023-02-05 17:07:47 -0800

    Notifee foreground service

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { AppNavigator } from './src/app/navigation/AppNavigator';
 import messaging from '@react-native-firebase/messaging';
 import StorybookUI from './storybook';
+import { Alert } from 'react-native';
+import { setNotificationForegroundService } from './src/app/backgroundTask/foregroundService';
 
 async function printDeviceFCMToken() {
     const getFcmToken = async () => {
@@ -26,6 +29,34 @@ async function printDeviceFCMToken() {
 printDeviceFCMToken();
 
 const App = () => {
+    useEffect(() => {
+        messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
+            console.log(remoteMessage);
+
+            let message_body = remoteMessage.notification.body;
+            let message_title = remoteMessage.notification.title;
+            let avatar = remoteMessage.notification.android.imageUrl;
+
+            Alert.alert(message_title, message_body);
+        });
+    }, []);
+
+    useEffect(() => {
+        const subscribe = messaging().onMessage(async (remoteMessage: any) => {
+            console.log(remoteMessage);
+
+            let message_body = remoteMessage.notification.body;
+            let message_title = remoteMessage.notification.title;
+            let avatar = remoteMessage.notification.android.imageUrl;
+
+            Alert.alert(message_title, message_body);
+        });
+
+        return subscribe;
+    }, []);
+
+    setNotificationForegroundService();
+
     return <AppNavigator />;
 };
 
