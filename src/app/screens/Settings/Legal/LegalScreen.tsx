3ec3ad0e3a2a4d8348bee39e04427d7614a91439
stellaw1 <stella01wang@gmail.com>
2023-03-14 00:41:31 -0700

    saves device id to local storage and adds server fetch fxn

--- src/app/screens/Settings/Legal/LegalScreen.tsx
+++ src/app/screens/Settings/Legal/LegalScreen.tsx
@@ -1,45 +1,14 @@
 import React, { useContext, useEffect } from 'react';
-import messaging from '@react-native-firebase/messaging';
 import { Text, View, StyleSheet, ScrollView, Button } from 'react-native';
 import { AppContext } from '../../../backgroundMode/context/AppContext';
 import { BackgroundMode } from '../../../backgroundMode/models/BackgroundMode';
 import Colours from '../../../constants/Colours';
-import { TriggerCall } from '../../../EMSCall/TriggerCall';
-import { EmergencyProtocolStack } from '../../../navigation/EmergencyProtocolStack';
-import { Alert } from 'react-native';
 
 interface Props {
     navigation: any;
 }
 
 export const LegalScreen = ({ navigation }: Props) => {
-    // useEffect(() => {
-    //     messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
-    //         console.log(remoteMessage);
-
-    //         let message_body = remoteMessage.notification.body;
-    //         let message_title = remoteMessage.notification.title;
-
-    //         Alert.alert(message_title, message_body);
-    //         navigation.navigate('EmergencyProtocol');
-    //         // TriggerCall();
-    //     });
-    // }, []);
-
-    // useEffect(() => {
-    //     const subscribe = messaging().onMessage(async (remoteMessage: any) => {
-    //         console.log(remoteMessage);
-
-    //         let message_body = remoteMessage.notification.body;
-    //         let message_title = remoteMessage.notification.title;
-
-    //         Alert.alert(message_title, message_body);
-    //         navigation.navigate('EmergencyProtocol');
-    //         // TriggerCall();
-    //     });
-
-    //     return subscribe;
-    // }, []);
     const { dispatch } = useContext(AppContext);
 
     return (
