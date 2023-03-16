2ba947e0e73f69c4dab480552fe5ac220103e27a
Emily Lukas <41706038+emilylukas@users.noreply.github.com>
2023-03-16 14:54:59 -0700

    Onboarding and Settings: Input type checking (#65)
    * Add type checking to onboarding inputs
    
    * Add type checking to settings

--- src/app/localStorage/hooks/useLocalStorage.ts
+++ src/app/localStorage/hooks/useLocalStorage.ts
@@ -7,6 +7,7 @@ import {
 } from '../../utils/formatValidators';
 import { LocalStorage } from '../LocalStorage/LocalStorage';
 import { LocalStorageCache } from '../LocalStorageCache/LocalStorageCache';
+import { FormError } from '../../utils/FormError';
 
 export const APP_DATA_STORAGE = 'APP_DATA_STORAGE';
 export const CARDIAC_STORAGE = 'CARDIAC_STORAGE';
@@ -20,9 +21,14 @@ export const useLocalStorage = () => {
     const isLocalStorageEmpty =
         appDataStorage.isEmpty() && cardiacStorage.isEmpty();
 
-    const saveUserBirthday = (birthday: string) => {
+    const saveUserBirthday = (birthday: string): undefined | FormError => {
         if (isBlank(birthday) || isBirthdayValid(birthday)) {
             appDataStorage.add(PersonalDataKeys.BIRTHDAY, birthday);
+        } else {
+            return new FormError(
+                PersonalDataKeys.BIRTHDAY,
+                'Please enter your birthday in DD/MM/YYYY format'
+            );
         }
     };
 
@@ -30,9 +36,23 @@ export const useLocalStorage = () => {
         appDataStorage.add(key, name);
     };
 
-    const saveUserWeightHeight = (key: string, num: string) => {
+    const saveUserWeightHeight = (
+        key: string,
+        num: string
+    ): undefined | FormError => {
         if (isBlank(num) || isPositiveNum(num)) {
             appDataStorage.add(key, num);
+        } else {
+            if (key === PersonalDataKeys.HEIGHT) {
+                return new FormError(
+                    PersonalDataKeys.HEIGHT,
+                    'Please enter a positive number for height'
+                );
+            }
+            return new FormError(
+                PersonalDataKeys.WEIGHT,
+                'Please enter a positive number for weight'
+            );
         }
     };
 
