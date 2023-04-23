d81320cfb65c2d77ebf8178dfa47b3f4e1c12737
stellaw1 <stella01wang@gmail.com>
2023-04-23 14:21:15 -0700

    updates ec2 ip address

--- env_vars.tsx
+++ env_vars.tsx
@@ -7,4 +7,4 @@ export enum AppModes {
 // SET THESE
 export const APP_MODE: AppModes = AppModes.DEFAULT_MODE;
 export const EMS_PHONE_NUMBER = '6047778888';
-export const EC2_IP_ADDRESS = 'http://34.209.155.128:3000';
+export const EC2_IP_ADDRESS = 'http://54.214.98.227:3000';
